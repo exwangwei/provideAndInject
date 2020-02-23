a2bba99eff0d6716d914cef7d8168d27e61fd920
Dwyane0801 <wangwei_08@163.com>
2020-02-23 19:46:16 +0800

    提交代码

---
+++ src/views/project/ChildrenF.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="border2">
+    <h3 :style="{ color: theme1.color }">F 节点</h3>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  inject: {
+    theme1: {  //为了避免名字重复起的别名
+      from: "theme",  //真名在这个from中
+      default: () => ({})
+    }
+  }
+};
+</script>
---
+++ src/views/project/ChildrenG.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="border2">
+    <h3>G 节点</h3>
+  </div>
+</template>
+<script>
+export default {};
+</script>
